678641afdeab6f0c21076c5ef269baec4b0f34b5
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-06-01 20:29:55 -0700

    Completed Chapter 2, Creating a book api

--- library/config/settings.py
+++ library/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,10 @@ INSTALLED_APPS = [
 
     # Local
     'books',
+    'api',
+
+    # Third party
+    'rest_framework', 
 ]
 
 MIDDLEWARE = [
@@ -57,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
